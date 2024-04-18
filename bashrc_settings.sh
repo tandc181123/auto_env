7424704ad90b371a7d18006bd672d70afb98fd2c
Tommy <153081087+tandc181123@users.noreply.github.com>
2024-04-18 15:36:05 +0800

    feat(cd): refactor with case statement, add prompt
    Refactor the cd function using a case statement for better clarity.
    Add a prompt for environment activation.

--- bashrc_settings.sh
+++ bashrc_settings.sh
@@ -11,13 +11,20 @@ cd_func() {
 }
 
 switch() {
-    if [[ -n "${VIRTUAL_ENV}" ]]; then
-        if [[ "$(pwd)" != "${FOLDER_PATH}" && ! "$(pwd)" =~ ^"${FOLDER_PATH}/".* ]]; then
+    case "$VIRTUAL_ENV" in
+    "")
+        if [[ -n "$folder_path" && -n "$virtual_env_name" ]]; then
+            if [[ "$(pwd)" == "$folder_path" || "$(pwd)" =~ ^"${folder_path}/".* ]]; then
+                source "${folder_path}/${virtual_env_name}/bin/activate"
+                echo -e "Virtual environment '$virtual_env_name' has been activated \e[32m\u2713\e[0m"
+            fi
+        fi
+        ;;
+    *)
+        if [[ "$(pwd)" != "$folder_path" && ! "$(pwd)" =~ ^"${folder_path}/".* ]]; then
             deactivate
+            echo -e "Virtual environment '$virtual_env_name' has been deactivated \e[31m\u2717\e[0m"
         fi
-    elif [[ -z "${VIRTUAL_ENV}" && -n "${FOLDER_PATH}" ]]; then
-        if [[ "$(pwd)" == "${FOLDER_PATH}" || "$(pwd)" =~ ^"${FOLDER_PATH}/".* ]]; then
-            source "${FOLDER_PATH}/venv/bin/activate"
-        fi
-    fi
+        ;;
+    esac
 }
